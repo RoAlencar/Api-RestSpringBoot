b489956f133af4ea0b68e7ffa71e424908520970
RoAlencar <roalencarx@gmail.com>
2022-03-03 11:53:52 -0300

    Api-RestSpringBoot feat: Implementando os métodos GET e POST

---
+++ src/main/java/br/com/raxinformatica/apirest/resources/ProdutoResource.java
@@ -0,0 +1,31 @@
+package br.com.raxinformatica.apirest.resources;
+
+import br.com.raxinformatica.apirest.models.Produto;
+import br.com.raxinformatica.apirest.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value="/api")
+public class ProdutoResource {
+
+    @Autowired
+    ProdutoRepository produtoRepository;
+
+    @GetMapping("/produtos")
+    public List<Produto> listaProdutos(){
+        return produtoRepository.findAll();
+    }
+
+    @GetMapping("/produto/{id}")
+    public Produto listaProdutoUnico(@PathVariable(value="id") long id){
+        return  produtoRepository.findById(id);
+    }
+
+    @PostMapping("/produto")
+    public Produto salvaProduto(@RequestBody Produto produto){
+        return produtoRepository.save(produto);
+    }
+}
